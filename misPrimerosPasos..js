fd3f95f1cc5d487eba5c2f11b5c2fd92669d2285
TOMMYLOS <torresurban@gmail.com>
2022-10-15 14:24:49 -0500

    actualizando archivo

--- misPrimerosPasos..js
+++ misPrimerosPasos..js
@@ -6,3 +6,18 @@ const b = 454
 console.log(a+b)
 
 console.log('mi segundo dia de practica,')
+
+console.log(Math.pow(a,2))
+
+let belen = 'Pozo Torres'
+let apellido = 'Pozo Torres'
+
+apellido
+
+belen
+
+
+var manzana = 'la manzana es roja y dulce'
+manzana
+
+console.log(manzana)
