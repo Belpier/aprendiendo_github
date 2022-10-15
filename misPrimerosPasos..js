69ceb9b44dc2d00701691cc076581e4403fa7617
TOMMYLOS <torresurban@gmail.com>
2022-10-15 15:21:27 -0500

    avanses

--- misPrimerosPasos..js
+++ misPrimerosPasos..js
@@ -11,6 +11,7 @@ console.log(Math.pow(a,2))
 
 let belen = 'Pozo Torres'
 let apellido = 'Pozo Torres'
+console.log (apellido.length)
 
 apellido
 
@@ -21,3 +22,28 @@ var manzana = 'la manzana es roja y dulce'
 manzana
 
 console.log(manzana)
+var comida ='aji de gallina'
+comida
+console.log(`a mi mmama le gusta el ${comida}`)
+console.log('a mi mama le gusta el ${comida}')
+console.log( Math.pow(7,3))
+console.log( Math.pow(4,5))
+console.log( Math.pow(3,2))
+console.log( Math.pow(8,4))
+console.log( Math.pow(3,9))
+console.log(Math.pow(6,6))
+console.log(Math.pow(7,5))
+console.log(Math.pow(9,4))
+console.log(Math.pow(8,4))
+console.log(Math.pow(3,7))
+const bailar=false
+
+console.log(bailar)
+const z = 21 % 5
+console.log(z)
+
+function sumarDosNumeros(a,b){
+    var sumar = a+b
+    return sumar
+}
+console.log(sumarDosNumeros(7,9))
